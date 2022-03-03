8e29024384430ab99d4a075af8d461a28ac55e30
deepakverma91123 <deepakverma91123@gmail.com>
2022-03-04 00:07:29 +0530

    update

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+// const errorHandler = require('./helpers/errorHandler');
+// const authJwt = require('./helpers/jwt');
+require('dotenv/config');
+require('./db/config')
+
+
+app.use(cors());
+app.options('*', cors())
+
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+// app.use(authJwt());
+// app.use((err, req, res, next)=> {
+//     if (err.name === 'UnauthorizedError') {
+//         res.status(401).json({
+//             message: 'User is not authorized'
+//         })
+//     }
+// });
+
+//Routes
+ 
+const userRoutes = require('./routes/user');
+ 
+
+// api routers
+ 
+app.use('/api/v1/users', userRoutes);
+ 
+
+// port
+const PORT = process.env.PORT || 8000
+app.listen(PORT, ()=> {
+    console.log(`Server is running on ${PORT}`);
+})
